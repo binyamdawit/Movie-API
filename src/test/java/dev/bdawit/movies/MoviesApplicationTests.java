2a6c90ce84652a911882fe245c364d7c51dcd3ca
Binyam <binyamdawit17@gmail.com>
2023-02-23 10:52:48 -0600

    First commit

---
+++ src/test/java/dev/bdawit/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bdawit.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
